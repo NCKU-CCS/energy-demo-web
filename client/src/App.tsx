671298170a4d8bc20f6710b14ba608957a9cab4d
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2018-11-07 01:25:48 +0800

    Test socket

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,36 @@
 import * as React from 'react';
+import io from 'socket.io-client';
+
+const socketURI = 'http://localhost:3000';
+
+interface IState {
+  egauge?: {
+    dataid: string;
+    use: number;
+    gen: number;
+    grid: number;
+    createdAt: string;
+  }
+}
 
 class App extends React.Component {
-  public render() {
+  state: IState = {};
+
+  componentDidMount() {
+    const socket = io(socketURI);
+    socket.on('egaugeAdded', (egauge: any) => this.setState({ egauge }));
+  }
+
+  render() {
+    // tslint:disable
+    console.log(this.state);
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">DEMO</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+        <p>
+          Egauge Data: { JSON.stringify(this.state.egauge) }
         </p>
       </div>
     );
