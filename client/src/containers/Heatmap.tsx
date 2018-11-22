bf5ffc6cabcc6119e29392c32b4975aaa6eded02
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2018-11-22 15:51:33 +0800

    Add Heatmap

---
+++ client/src/containers/Heatmap.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import { Row } from 'antd';
+
+// @ts-ignore
+import HeatmapOverlay from 'react-map-gl-heatmap-overlay';
+import MapLayer from '../components/MapLayer';
+
+class Heatmap extends React.Component {
+  render() {
+    return (
+      <Query
+        query={query}
+        variables={{
+          filter: {
+          // FIX: use today
+          createdAt_gte: '2015/1/30',
+        }}}
+      >{({ loading, error, data }) => {
+        if (loading) {
+          return (<div />);
+        }
+        return (
+          <React.Fragment>
+            <Row style={{ backgroundColor: '#fff', padding: 10 }}>
+              <MapLayer render={(viewport) => (
+                <HeatmapOverlay
+                  {...viewport}
+                  locations={data.houses || []}
+                  intensityAccessor={(house: any) => house.reducedEgauge ? house.reducedEgauge.powerUsage : 0} 
+                  lngLatAccessor={(house: any) => [house.lng, house.lat ]}
+                />
+                )}
+              />
+            </Row>
+          </React.Fragment>
+        )
+      }}
+    </Query>
+    );
+  }
+};
+
+const query = gql`
+query getHouses($filter: Filter) {
+  houses: Houses(filter: $filter) {
+    dataid,
+    lat,
+    lng,
+    reducedEgauge {
+      powerUsage
+    }
+  }
+}
+`;
+
+export default Heatmap;
